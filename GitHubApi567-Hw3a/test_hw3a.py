90f7f99350d8036906e9bbcedfab7abcb1e05fb2
Kyle <kyleponte07@gmail.com>
2025-09-29 23:25:14 -0400

    Updated tests to utilize mocking

--- GitHubApi567-Hw3a/test_hw3a.py
+++ GitHubApi567-Hw3a/test_hw3a.py
@@ -1,5 +1,6 @@
 import unittest
 import hw3a
+from unittest.mock import patch
 
 
 class FakeResponse:
@@ -11,19 +12,32 @@ class FakeResponse:
         return self._json
 
 class TestHW3AMock(unittest.TestCase):
-    def test_known_user_has_repos(self):
-        """Check that a known public user has at least one repo."""
-        data = hw3a.list_repos("kyleponte")
-        self.assertIsInstance(data, list)
-        self.assertGreater(len(data), 0)
-        for repo in data:
-            self.assertIn("repo", repo)
-            self.assertIn("commits", repo)
-
-    def test_fake_user_raises(self):
-        """Check that an obviously fake user raises ValueError."""
+    @patch("hw3a.requests.get")
+    def test_user_not_found(self, mock_get):
+        """ValueError should be raised."""
+        mock_get.return_value = FakeResponse(status_code=404, json_data=[])
         with self.assertRaises(ValueError):
-            hw3a.list_repos("kyleponte12736172301923")
+            hw3a.list_repos("kyleponte10122371238")
+
+    @patch("hw3a.requests.get")
+    def test_repo_with_commits(self, mock_get):
+        """Repo with two commits should return commit_count=2."""
+        mock_get.side_effect = [
+            FakeResponse(200, [{"name": "RepoA"}]),                 
+            FakeResponse(200, [{"test": "1"}, {"test": "2"}])      
+        ]
+        out = hw3a.list_repos("Kyle")
+        self.assertEqual(out, [{"repo": "RepoA", "commits": 2}])
+
+    @patch("hw3a.requests.get")
+    def test_repo_with_no_commits(self, mock_get):
+        """Repo with no commits should return commit_count=0."""
+        mock_get.side_effect = [
+            FakeResponse(200, [{"name": "RepoB"}]),  
+            FakeResponse(200, [])                    
+        ]
+        out = hw3a.list_repos("John")
+        self.assertEqual(out, [{"repo": "RepoB", "commits": 0}])
 
 
 if __name__ == "__main__":
