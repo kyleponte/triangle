e9e7c0d6360b7843beedb97f7b180d1f5c855ee0
Kyle <kyleponte07@gmail.com>
2025-10-02 19:50:03 -0400

    Fixed problems with program and test file

--- test_triangle.py
+++ test_triangle.py
@@ -5,11 +5,11 @@ class fooTest(unittest.TestCase):
     def test_triangle_1(self):
         t = classify_triangle(1, 2, 3)
         self.assertEqual(t, "Not a triangle")
-    
+
     def test_triangle_2(self):
         t = classify_triangle(0, 0, 0)
         self.assertEqual(t, "Not a triangle")
-    
+
     def test_triangle_3(self):
         t = classify_triangle(1, 5, 8)
         self.assertEqual(t, "Not a triangle")
@@ -17,11 +17,11 @@ class fooTest(unittest.TestCase):
     def test_equilateral_triangle_1(self):
         t = classify_triangle(1, 1, 1)
         self.assertEqual(t, "Equilateral")
-    
+
     def test_equilateral_triangle_2(self):
         t = classify_triangle(3, 3, 3)
         self.assertEqual(t, "Equilateral")
-    
+
     def test_equilateral_triangle_3(self):
         t = classify_triangle(5, 5, 5)
         self.assertEqual(t, "Equilateral")
@@ -29,11 +29,11 @@ class fooTest(unittest.TestCase):
     def test_isosceles_triangle_1(self):
         t = classify_triangle(5, 2, 5)
         self.assertEqual(t, "Isosceles")
-    
+
     def test_isosceles_triangle_2(self):
         t = classify_triangle(3, 5, 3)
         self.assertEqual(t, "Isosceles")
-    
+
     def test_isosceles_triangle_3(self):
         t = classify_triangle(7, 7, 10)
         self.assertEqual(t, "Isosceles")
@@ -41,7 +41,7 @@ class fooTest(unittest.TestCase):
     def test_scalene_triangle_1(self):
         t = classify_triangle(4, 2, 3)
         self.assertEqual(t, "Scalene")
-    
+
     def test_scalene_triangle_2(self):
         t = classify_triangle(5, 6, 7)
         self.assertEqual(t, "Scalene")
@@ -57,10 +57,15 @@ class fooTest(unittest.TestCase):
     def test_right_triangle_2(self):
         t = classify_triangle(3, 5, 4)
         self.assertEqual(t, "Right Scalene")
-    
+
     def test_right_triangle_3(self):
         t = classify_triangle(6, 8, 10)
         self.assertEqual(t, "Right Scalene")
+
+    def test_right_triangle_4(self):
+        t = classify_triangle(1, 1, 2 ** 0.5)
+        self.assertEqual(t, "Right Isosceles")
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
--- triangle.py
+++ triangle.py
@@ -1,21 +1,21 @@
+"""Triangle classifier: returns a string label given three side lengths."""
 def classify_triangle(a, b, c):
+    """Return a label such as 'Equilateral', 'Isosceles', 'Right Scalene', or 'Not a triangle'."""
     if a + b <= c or a + c <= b or b + c <= a:
         return 'Not a triangle'
-    
+
     sides = sorted([a, b, c])
     a = sides[0]
     b = sides[1]
     c = sides[2]
-    
+    tolerance = 1e-9
+
     if a == b and a == c:
         return 'Equilateral'
-    elif a == b or a == c or b == c:
-        if pow(a, 2) + pow(b, 2) == pow(c, 2):
-            return 'Right Isoceles'
-        else:
-            return 'Isosceles'
-    else:
-        if pow(a, 2) + pow(b, 2) == pow(c, 2):
-            return 'Right Scalene'
-        else:
-            return 'Scalene'
+    if a == b or a == c or b == c:
+        if abs((pow(a, 2) + pow(b, 2) - pow(c, 2)) - 0) <= tolerance:
+            return 'Right Isosceles'
+        return 'Isosceles'
+    if pow(a, 2) + pow(b, 2) == pow(c, 2):
+        return 'Right Scalene'
+    return 'Scalene'
